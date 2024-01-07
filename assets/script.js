9ce30f1a26000e8fba8296340e5ab6e3ddfc79eb
James Rchrdsn <laurent.james63@gmail.com>
2024-01-07 15:50:37 +0100

    Print It Ready

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,19 @@
 const slides = [
   {
-    image: "slide1.jpg",
+    image: "assets/images/slideshow/slide1.jpg",
     tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
   },
   {
-    image: "slide2.jpg",
+    image: "assets/images/slideshow/slide2.jpg",
     tagLine:
       "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
   },
   {
-    image: "slide3.jpg",
+    image: "assets/images/slideshow/slide3.jpg",
     tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
   },
   {
-    image: "slide4.png",
+    image: "assets/images/slideshow/slide4.png",
     tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
   },
 ];
@@ -21,16 +21,46 @@ const slides = [
 const arrowLeft = document.querySelector(".arrow_left");
 const arrowRight = document.querySelector(".arrow_right");
 const dotsContainer = document.querySelector(".dots");
+const img = document.querySelector(".allBanner img");
+const textLine = document.querySelector(".textLine");
 let dotSelect = 0;
 
-arrowLeft.addEventListener("click", () => {
-  console.log("gauche");
-});
+// Fonction du clique sur la fleche gauche avec le suivi
+// du dot au changement d'image
+function Left() {
+  arrowLeft.addEventListener("click", () => {
+    const slideDots = document.querySelectorAll(".dots .dot");
+    slideDots[dotSelect].classList.remove("dot_selected");
+    dotSelect--;
 
-arrowRight.addEventListener("click", () => {
-  console.log("droite");
-});
+    if (dotSelect < 0) {
+      dotSelect = 3;
+    }
+    slideDots[dotSelect].classList.add("dot_selected");
+    img.src = slides[dotSelect].image;
+    textLine.innerHTML = slides[dotSelect].tagLine;
+  });
+}
 
+// Fonction du clique sur la fleche droite avec le suivi
+// du dot au changement d'image
+function Right() {
+  arrowRight.addEventListener("click", () => {
+    const slideDots = document.querySelectorAll(".dots .dot");
+    slideDots[dotSelect].classList.remove("dot_selected");
+    dotSelect++;
+
+    if (dotSelect > slides.length - 1) {
+      dotSelect = 0;
+    }
+    slideDots[dotSelect].classList.add("dot_selected");
+    img.src = slides[dotSelect].image;
+    textLine.innerHTML = slides[dotSelect].tagLine;
+  });
+}
+
+// Fonction qui créer le nombre de dot en conséquence du nombre de slides
+// ainsi  que le dot actif
 function createDots() {
   for (let i = 0; i < slides.length; i++) {
     let bulletElement = document.createElement("a");
@@ -45,3 +75,5 @@ function createDots() {
 }
 
 createDots();
+Right();
+Left();
